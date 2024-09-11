0140d0498753d954a6d35e4d48e3b3e34206af04
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:36:12 +0200

    [UPD] Added forms

--- index.php
+++ index.php
@@ -8,7 +8,35 @@
 </head>
 
 <body>
-    
+<form action="answer.php" method="GET">
+        <div>
+            <div>
+                <label for="paragraph">Type something</label>
+            </div>
+
+            <div>
+                <textarea type="text" name="paragraph" id="paragraph"></textarea>
+            </div>
+        </div>
+
+        <hr>
+
+        <div>
+            <div>
+                <label for="censored">Type a word you want to censure</label>
+            </div>
+
+            <div>
+                <input type="text" name="censored" id="censored">
+            </div>
+        </div>
+
+        <hr>
+
+        <div>
+            <button>Invia il form</button>
+        </div>
+    </form>
 </body>
 
 </html>
