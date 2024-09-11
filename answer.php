b89315fa9a45e857c6c7cb9b4d54791b2ec2cf0d
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:37:04 +0200

    [UPD] Added html

--- answer.php
+++ answer.php
@@ -5,3 +5,15 @@ $censored = $_GET['censored'];
 $censored_hidden = str_replace($censored, '***', $paragraph);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+
+
+</html>
