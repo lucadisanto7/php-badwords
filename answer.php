a64b9323cc7d2af06b023bf2de436df070c68bc0
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:37:15 +0200

    [UPD] Added body

--- answer.php
+++ answer.php
@@ -13,7 +13,21 @@ $censored_hidden = str_replace($censored, '***', $paragraph);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 </head>
+<body>
+    <div>
+        <h2>Questo è il paragrafo inserito:</h2>
+        <p><?php echo $paragraph; ?></p>
+    </div>
 
+    <div>
+        <h3>Questa è la parola da censurare:</h3>
+        <p><?php echo $censored; ?></p>
+    </div>
 
+    <div>
+        <h3>Questo è il paragrafo con la parola censurata</h3>
+        <p><?php echo $censored_hidden; ?></p>
+    </div>
+</body>
 
 </html>
